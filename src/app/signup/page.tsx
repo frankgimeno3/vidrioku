c02ee08ecca0575c0f1a70752eaacea00cd16944
frank gimeno <frankgimeno3@gmail.com>
2023-10-27 11:34:23 +0200

    tipo descentralizado

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,6 +10,7 @@ import Contras from './signupComponents/Contras';
 
 import { collection, addDoc, getDoc, query, onSnapshot, deleteDoc, doc, } from 'firebase/firestore';
 import { db } from '../firebase';
+import Tipo from './signupComponents/Tipo';
 
 
 export default function Signup() {
@@ -94,24 +95,7 @@ export default function Signup() {
                       />
                     </div>
                   </div>
-                  <div>
-                    <label htmlFor="userType" className="block text-sm font-medium leading-6 text-white">
-                      Tipo de cuenta
-                    </label>
-                    <div className="mt-2">
-                      <select
-                        id="userType"
-                        name="userType"
-                        value={userType}
-                        onChange={handleUserTypeChange}
-                        required
-                        className="bg-transparent block w-full rounded-md border-0   py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                      >
-                        <option value="profesional" className='bg-gray-100 text-black'>Profesional</option>
-                        <option value="empresa" className='bg-gray-100 text-black'>Empresa</option>
-                      </select>
-                    </div>
-                  </div>
+                  <Tipo userType={userType} handleUserTypeChange={handleUserTypeChange}/>
                   <Contras setPassword={setPassword} setPasswordAgain={setPasswordAgain}/>
 
                   <div>
--- src/app/signup/signupComponents/Tipo.tsx
+++ src/app/signup/signupComponents/Tipo.tsx
@@ -3,17 +3,34 @@ import { FC } from 'react';
 
 
 interface TipoProps {
-
+    userType: any
+    handleUserTypeChange: any
 }
 
-const Tipo: React.FC<TipoProps> = ({  }) => {
+const Tipo: React.FC<TipoProps> = ({ userType, handleUserTypeChange }) => {
   const router = useRouter();
 
 
 
   return (
-     <></>
-  );
+    <div>
+    <label htmlFor="userType" className="block text-sm font-medium leading-6 text-white">
+      Tipo de cuenta
+    </label>
+    <div className="mt-2">
+      <select
+        id="userType"
+        name="userType"
+        value={userType}
+        onChange={handleUserTypeChange}
+        required
+        className="bg-transparent block w-full rounded-md border-0   py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+      >
+        <option value="profesional" className='bg-gray-100 text-black'>Profesional</option>
+        <option value="empresa" className='bg-gray-100 text-black'>Empresa</option>
+      </select>
+    </div>
+  </div>  );
 };
 
 export default Tipo;
