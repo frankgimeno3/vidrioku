de5beb57e4500c4a9ade3ecb7202b843e4bd8080
frank gimeno <frankgimeno3@gmail.com>
2023-11-04 21:20:41 +0100

    .

--- src/app/configuracion/page.tsx
+++ src/app/configuracion/page.tsx
@@ -14,12 +14,14 @@ const Configuracion: FC = ({ }) => {
   return (
     <>
       <Navbar />
-
       <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
         <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Configuración</h2>
         <div className=" mx-6  bg-white  h-full text-zinc-900">
-          <div className="p-5">
+        <div className="p-5">
             Cambiar mi contraseña
+          </div>
+          <div className="p-5">
+            Datos bancarios
           </div>
         </div>
       </div>
