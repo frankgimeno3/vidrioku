79f8b927b4f02e679ffd98f67934b0d55d1f4645
frank gimeno <frankgimeno3@gmail.com>
2023-11-14 20:21:36 +0100

    se renderizan las ofertas

--- src/app/ofertas/ofertascomponents/OfertasList.tsx
+++ src/app/ofertas/ofertascomponents/OfertasList.tsx
@@ -8,24 +8,33 @@ import Anuncio from './compListados/Anuncio';
 import Pasarela from './compListados/Pasarela';
 import Oferta from './compListados/Oferta';
 import Rendercomponent from './compListados/rendercomponent/Rendercomponent';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { Timestamp, collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/app/firebase';
 
 interface OfertasListProps {
- }
+}
 
- type Oferta = {
-  titulo: string;
-  cargo: string;
+type Oferta = {
+  titulo: string,
+  cargo: string,
+  jornada: string,
+  tipoubi: string,
+  ubicacion: string,
+  descripcion: string,
+  experiencia: string,
+  adicional: string,
+  empresa: string,
+  publicacion: Timestamp,
+  estado: string,
 };
 
-const OfertasList: FC<OfertasListProps> = ({   }) => {
+const OfertasList: FC<OfertasListProps> = ({ }) => {
   const router = useRouter();
   const [selectedComponent, setSelectedComponent] = useState("none")
   const [loading, setLoading] = useState(true);
   const [misOfertas, setMisOfertas] = useState<Oferta[]>([]);
 
-  const componentselectorhandler = ()=>{
+  const componentselectorhandler = () => {
     setSelectedComponent("nombredelcomponent")
   }
 
@@ -52,9 +61,9 @@ const OfertasList: FC<OfertasListProps> = ({   }) => {
   if (loading) {
     return <p>Cargando ofertas...</p>;
   }
-  
+
   return (
-    
+
     <div className="flex flex-col  min-h-screen bg-zinc-800 ">
 
       <nav className="bg-gray-200 py-2 px-1 text-center">
@@ -64,25 +73,23 @@ const OfertasList: FC<OfertasListProps> = ({   }) => {
       <div className='flex flex-col   mx-12 bg-white '>
         <div className='bg-white flex flex-row w-full h-screen'>
           <div className='flex flex-col flex-1 justify-between h-full'>
-                          {/* <Anuncio />
+            {/* <Anuncio />
               <Pasarela /> */}
             <ul className='max-h-full overflow-scroll'>
-              <Oferta />
-              <Oferta />
-
-              <Oferta />
-              <Oferta />
-
-              <Oferta />
-              <Oferta />
-              
+              {misOfertas.map((oferta, index) => (
+                <Oferta key={index} titulo={oferta.titulo} cargo={oferta.cargo} jornada={oferta.jornada} tipoubi={oferta.tipoubi}
+                 ubicacion={oferta.ubicacion} descripcion={oferta.descripcion} experiencia={oferta.experiencia} adicional={oferta.adicional} 
+                 empresa={oferta.empresa}  estado={oferta.estado}
+                //  publicacion={oferta.publicacion}
+                 />
+              ))}
             </ul>
             <nav className="bg-gray-200 py-2 px-1 text-center ">
               <PageListButtons />
             </nav>
           </div>
           <div className='flex-1 h-full bg-gray-100 p-5'>
-            <Rendercomponent selectedComponent={selectedComponent}/>
+            <Rendercomponent selectedComponent={selectedComponent} />
           </div>
 
         </div>
--- src/app/ofertas/ofertascomponents/compListados/Oferta.tsx
+++ src/app/ofertas/ofertascomponents/compListados/Oferta.tsx
@@ -1,20 +1,33 @@
 import { FC } from 'react';
 import Image from 'next/image';
 
+interface OfertaProps {
+  titulo: string,
+  cargo: string,
+  jornada: string,
+  tipoubi: string,
+  ubicacion: string,
+  descripcion: string,
+  experiencia: string,
+  adicional: string,
+  empresa: string,
+  // publicacion: Timestamp,
 
-const Oferta: FC = () => {
+   estado: string,
+}
+const Oferta: FC <OfertaProps>= ({titulo, cargo, jornada, tipoubi, ubicacion, descripcion, experiencia, adicional, empresa, estado}) => {
   return (
     <div className="flex flex-row justify-left items-center p-5 bg-gray-50 hover:bg-gray-100 shadow-lg mb-1 text-gray-600">
       <Image src={"/inventedlogos/1.png"} alt="pepo" height={75} width={75} />
       <div className='justify-left pl-5 w-full'>
-        <h2>Título de la oferta</h2>
+        <h2>{titulo}</h2>
         <div className='flex flex-row justify-between w-full'>
           <div className='flex flex-col text-sm text-gray-500'>
-            <p>Empresa</p>
-            <p>Lugar</p>
+            <p>{empresa}</p>
+            <p>{ubicacion}</p>
           </div>
           <div className='flex flex-row'>
-            <button> Nombre de la posición</button>
+            <button> {cargo}</button>
           </div>
         </div>
 
