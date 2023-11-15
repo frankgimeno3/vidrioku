a226fc5918ee954e0d4b7b0b41f8cb9d6520726a
frank gimeno <frankgimeno3@gmail.com>
2023-11-15 12:54:07 +0100

    creo solicitar y modifico el render para comenzar ruta dinamica

--- src/app/ofertas/ofertascomponents/compListados/rendercomponent/Rendercomponent.tsx
+++ src/app/ofertas/ofertascomponents/compListados/rendercomponent/Rendercomponent.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Image from 'next/image';
 import Descripcion from '@/app/crearoferta/componentes/Descripcion';
+import { useRouter } from 'next/navigation';
 interface RendercomponentProps {
     titulo: string,
     cargo: string,
@@ -11,41 +12,50 @@ interface RendercomponentProps {
     experiencia: string,
     adicional: string,
     empresa: string,
-     estado: string,
-  }
+    estado: string,
+}
 
-const Rendercomponent: FC <RendercomponentProps> = ({titulo, cargo, jornada, tipoubi, ubicacion, descripcion, experiencia,
-    adicional, empresa, estado}) => {
+const Rendercomponent: FC<RendercomponentProps> = ({ titulo, cargo, jornada, tipoubi, ubicacion, descripcion, experiencia,
+    adicional, empresa, estado }) => {
+    const router = useRouter();
+
+    const handleSolicitarEmpleo = () => {
+        const queryParams = `?titulo=${titulo}&cargo=${cargo}&jornada=${jornada}&ubicacion=${ubicacion}&descripcion=${descripcion}&experiencia=${experiencia}&adicional=${adicional}&empresa=${empresa}&estado=${estado}`;
+        router.push(`/solicitar${queryParams}`);
+    };
+
     return (
         <div className='flex flex-col bg-gray-50 shadow-lg h-full text-center items-center w-full text-gray-500 py-8 px-24 overflow-scroll'>
             <Image src={"/inventedlogos/1.png"} alt="pepo" height={100} width={100} />
             <h2 className='mt-5 text-xl'>{titulo}</h2>
             <div className='flex flex-col text-sm text-gray-500'>
-            <p>{cargo}</p>
-            <p>{empresa}</p>
+                <p>{cargo}</p>
+                <p>{empresa}</p>
                 <p>{ubicacion}</p>
             </div>
             <p className='text-sm mt-5'>
                 Descripción de la oferta
             </p>
             <p className='text-sm mt-1'>
-{descripcion}            </p>
+                {descripcion}            </p>
             <p className='text-sm mt-5'>
                 Requerimientos
             </p>
             <p className='text-sm mt-1'>
-{experiencia}            </p>
+                {experiencia}            </p>
             <p className='text-sm mt-5'>
-               Tipo de jornada
+                Tipo de jornada
             </p>
             <p className='text-sm mt-1'>
-{jornada}            </p>
+                {jornada}            </p>
             <p className='text-sm mt-5'>
                 Detalles adicionales
             </p>
             <p className='text-sm mt-1'>
-{adicional}            </p>
-            <button className='p-2 border shadow-lg rounded-lg text-xs mt-5'> Solicitar Empleo</button>
+                {adicional}            </p>
+            <button className='p-2 border shadow-lg rounded-lg text-xs mt-5' 
+            onClick={handleSolicitarEmpleo}>
+                Solicitar Empleo</button>
 
         </div>
 
---
+++ src/app/solicitar/page.tsx
@@ -0,0 +1,109 @@
+"use client"
+import { FC, useEffect, useState } from 'react';
+import { redirect, useRouter } from 'next/navigation';
+import Navbar from '../components/Navbar';
+import { useSession } from 'next-auth/react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../firebase';
+
+type Oferta = {
+  titulo: string;
+  cargo: string;
+};
+
+const Solicitar: FC = () => {
+  const router = useRouter();
+  const [userData, setUserData] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [misOfertas, setMisOfertas] = useState<Oferta[]>([]);
+
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/signin');
+    },
+  });
+
+  useEffect(() => {
+    if (session?.data?.user?.email) {
+      setUserData(session.data.user.email);
+    } else {
+      setUserData("Usuario");
+    }
+  }, [session]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true); // Indicar que se está cargando
+
+      const ofertasCollection = collection(db, 'ofertas');
+      const q = query(ofertasCollection, where('empresa', '==', userData));
+      const querySnapshot = await getDocs(q);
+
+      const offersData: Oferta[] = [];
+      querySnapshot.forEach((doc) => {
+        offersData.push(doc.data() as Oferta);
+      });
+
+      setMisOfertas(offersData);
+      setLoading(false); // Indicar que la carga ha finalizado
+    };
+
+    fetchData();
+  }, [userData]);
+
+  if (loading) {
+    return <p>Cargando ofertas...</p>;
+  }
+
+  const nuevaofertahandler = ()=>{
+    router.push("/crearoferta")
+  }
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
+        <div className='flex flex-row justify-between py-3 bg-zinc-800 bg-opacity-50 px-60'>
+          <h2 className="   font-bold text-lg   ">Mis Ofertas</h2>
+          <button className='bg-white rounded-lg px-3 py-1 text-sm text-gray-500'
+          onClick={nuevaofertahandler}>Crear nueva oferta</button>
+        </div>
+        <div className="p-5 bg-white bg-opacity-10 ">
+          <h2 className='ml-56'>Ofertas activas</h2>
+          {misOfertas.map((oferta, index) => (
+            <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
+              <h3 className='font-medium'>{oferta.titulo}</h3>
+              <p>{oferta.cargo}</p>
+              <div className='flex flex-row justify-center pt-3'>
+                <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Editar oferta</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver perfiles recomendados</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Desactivar oferta</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="p-5 bg-white bg-opacity-10 ">
+          <h2 className='ml-56'>Ofertas inactivas</h2>
+          {misOfertas.map((oferta, index) => (
+            <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
+              <h3 className='font-medium'>{oferta.titulo}</h3>
+              <p>{oferta.cargo}</p>
+              <div className='flex flex-row justify-center pt-3'>
+                <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Reactivar oferta</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes</button>
+                <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Solicitar;
