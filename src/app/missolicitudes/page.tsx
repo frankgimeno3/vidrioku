c4d69ab57e4597f2519ccbcec5e65ce542cdb631
frank gimeno <frankgimeno3@gmail.com>
2023-11-16 00:36:44 +0100

    creo mis solicitudes

---
+++ src/app/missolicitudes/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function misSolicitudes() {
+  return (
+    <div>empresas</div>
+  )
+}
+
+misSolicitudes.propTypes = {}
+
+export default misSolicitudes
--- src/app/solicitar/(solicitar)/[id]/page.tsx
+++ src/app/solicitar/(solicitar)/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { FC, useEffect, useState } from 'react';
- import Navbar from '../../../components/Navbar'
+import Navbar from '../../../components/Navbar'
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/app/firebase';
+import Image from "next/image"
+import Link from 'next/link';
 
-interface SolicitudProps   {
-  params: {id: string}
+interface SolicitudProps {
+  params: { id: string }
 }
 
 type OfertaProps = {
@@ -18,11 +20,11 @@ type OfertaProps = {
   experiencia: string,
   adicional: string,
   empresa: string,
-   estado: string,
+  estado: string,
   id: any
 };
 
-const Solicitud: FC <SolicitudProps>=  ({params}) => {
+const Solicitud: FC<SolicitudProps> = ({ params }) => {
   const [loading, setLoading] = useState(true);
   const [oferta, setOferta] = useState<OfertaProps>();
 
@@ -30,7 +32,7 @@ const Solicitud: FC <SolicitudProps>=  ({params}) => {
     const fetchData = async () => {
       setLoading(true);
       const ofertasCollection = collection(db, 'ofertas');
-      const q = query(ofertasCollection, where('id', '==', params.id));  
+      const q = query(ofertasCollection, where('id', '==', params.id));
       const querySnapshot = await getDocs(q);
 
       querySnapshot.forEach((doc) => {
@@ -42,20 +44,56 @@ const Solicitud: FC <SolicitudProps>=  ({params}) => {
 
     fetchData();
   }, [params.id]);
-    return (
-<>
-      <Navbar   />
+  return (
+    <>
+      <Navbar />
       <div className="flex flex-col  max-h-screen bg-zinc-800 ">
 
-<div className='flex flex-col   mx-12 bg-white '>
-  <div className='bg-white flex flex-row w-full h-screen'>
-    <h3 className='bg-red-200 text-black'>{oferta?.descripcion}</h3>
- 
-  </div>
-  </div>
-  </div>
-</>
-   )
-   ;
+        <div className='flex flex-col   mx-12 bg-white '>
+          <div className='bg-white flex flex-row w-full h-screen'>
+            <div className="flex flex-col bg-gray-50 shadow-lg h-full text-center items-center w-full text-gray-500 py-8 px-24 overflow-scroll">
+              <Image src={"/inventedlogos/1.png"} alt="pepo" height={100} width={100} />
+              <h2 className="mt-5 text-xl">{oferta?.titulo}</h2>
+              <div className="flex flex-col text-sm text-gray-500">
+                <p>{oferta?.cargo}</p>
+                <p>{oferta?.empresa}</p>
+                <p>{oferta?.ubicacion}</p>
+              </div>
+              <p className="text-sm mt-5">
+                Descripción de la oferta
+              </p>
+              <p className="text-sm mt-1">
+                {oferta?.descripcion}
+              </p>
+              <p className="text-sm mt-5">
+                Requerimientos
+              </p>
+              <p className="text-sm mt-1">
+                {oferta?.experiencia}
+              </p>
+              <p className="text-sm mt-5">
+                Tipo de jornada
+              </p>
+              <p className="text-sm mt-1">
+                {oferta?.jornada}
+              </p>
+              <p className="text-sm mt-5">
+                Detalles adicionales
+              </p>
+              <p className="text-sm mt-1">
+                {oferta?.adicional}
+              </p>
+              <Link href={`/`}>
+                <button className="p-2 border shadow-lg rounded-lg text-xs mt-5">
+                  Enviar solicitud
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+    ;
 }
 export default Solicitud;    
