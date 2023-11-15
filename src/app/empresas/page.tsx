105f368ad100d738bba449d132589129f8534e6a
frank gimeno <frankgimeno3@gmail.com>
2023-11-16 00:06:22 +0100

    soluciono  is not a module

--- src/app/empresas/page.tsx
+++ src/app/empresas/page.tsx
@@ -1,2 +1,12 @@
-// aquí tine que ir el perfil de empresa como ruta dinámica []
-//     se usa un id de empresa
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function empresas(props) {
+  return (
+    <div>empresas</div>
+  )
+}
+
+empresas.propTypes = {}
+
+export default empresas
--- src/app/ofertas/ofertascomponents/compListados/rendercomponent/Rendercomponent.tsx
+++ src/app/ofertas/ofertascomponents/compListados/rendercomponent/Rendercomponent.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { FC } from "react";
 import Image from "next/image";
 import Descripcion from "@/app/crearoferta/componentes/Descripcion";
@@ -31,8 +32,7 @@ const Rendercomponent: FC<RendercomponentProps> = ({
     empresa,
     estado,
 }) => {
-    const router = useRouter();
-
+ 
     return (
         <div className="flex flex-col bg-gray-50 shadow-lg h-full text-center items-center w-full text-gray-500 py-8 px-24 overflow-scroll">
             <Image src={"/inventedlogos/1.png"} alt="pepo" height={100} width={100} />
