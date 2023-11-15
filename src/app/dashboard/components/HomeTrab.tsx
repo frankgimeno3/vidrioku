2d9bd232b38eb8635b65e7c942b3ab9c16c24a10
frank gimeno <frankgimeno3@gmail.com>
2023-11-16 00:39:35 +0100

    añado boton de mis solicitudes en la home

--- src/app/dashboard/components/HomeTrab.tsx
+++ src/app/dashboard/components/HomeTrab.tsx
@@ -21,7 +21,10 @@ interface User {
 const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
   const router = useRouter();
   const [user, setUser] = useState<User>();
- 
+
+  const handlemissolicitudes = ()=>{
+    router.push("/missolicitudes")
+  }
   useEffect(() => {
     const fetchDoc = async () => {
       if (userData) {
@@ -29,8 +32,8 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
         const response = await getDoc(docRef);
         if (response.exists()) {
           const myUserData = response.data() as User;
-          setUser(myUserData );
-         }
+          setUser(myUserData);
+        }
       }
     };
 
@@ -40,32 +43,32 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
   return (
     <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
       <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">Saludos, {user?.nombre}</h2>
-          <div   className=" w-full flex justify-between bg-gradient-to-b from-slate-900 to-slate-600">
-            <div className="flex flex-col p-4 w-full flex justify-between text-center justify-center px--auto">
-              <Image src="/icons/empty-user-profile.png" alt="" width={200} height={200} className="mx-auto my-5" />
-              <div className="flex flex-row mx-auto">
-                <span className="mr-1">{user?.nombre}</span>
-                <span className="capitalize">{user?.apellidos}</span>
-              </div>
-              <div className="flex flex-row mx-auto">
-                <span className='mr-1'>{user?.edad} </span>
-                 <span className="capitalize">({user?.genero})</span>
-              </div>
-              <span>{user?.ubi}</span>
-              <span>{userData}</span>
-              <div >
-                <button
-                  className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
-                >Mi currículum</button>
-              </div>
-              <div >
-                <button
-                  className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
-                >Editar información de mi perfil</button>
-              </div>
-            </div>
+      <div className=" w-full flex justify-between bg-gradient-to-b from-slate-900 to-slate-600">
+        <div className="flex flex-col p-4 w-full flex justify-between text-center justify-center px--auto">
+          <Image src="/icons/empty-user-profile.png" alt="" width={200} height={200} className="mx-auto my-5" />
+          <div className="flex flex-row mx-auto">
+            <span className="mr-1">{user?.nombre}</span>
+            <span className="capitalize">{user?.apellidos}</span>
           </div>
- 
+          <div className="flex flex-row mx-auto">
+            <span className='mr-1'>{user?.edad} </span>
+            <span className="capitalize">({user?.genero})</span>
+          </div>
+          <span>{user?.ubi}</span>
+          <span>{userData}</span>
+          <div >
+            <button
+              className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
+            >Mi currículum</button>
+          </div>
+          <div >
+            <button
+              className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
+            >Editar información de mi perfil</button>
+          </div>
+        </div>
+      </div>
+
       <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">Qué te has perdido?</h2>
       <div className='flex flex-row bg-gradient-to-b from-slate-900 to-slate-600 h-full'>
         <div className='h-full flex-1 text-center my-3'>
@@ -75,14 +78,16 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
           <p className='font-bold'>Publicaciones nuevas de las empresas que sigues</p>
         </div>
         <div className='h-full flex-1 text-center my-3'>
-        <p className='font-bold'>Han visto tu currículum</p>
-        <p>2 empresas esta semana</p>
-        <p>4 empresas este mes</p>
+          <p className='font-bold'>Solicitudes</p>
+          <button
+            className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
+            onClick={handlemissolicitudes}
+          >Mis solicitudes</button>
         </div>
         <div className='h-full flex-1 text-center my-3'>
-         <p className='font-bold'>Solicitudes enviadas</p>
-        <p>Solicitudes leídas</p>
-        <p>Solicitudes no leídas</p>
+          <p className='font-bold'>Solicitudes enviadas</p>
+          <p>Solicitudes leídas</p>
+          <p>Solicitudes no leídas</p>
         </div>
       </div>
     </div>
