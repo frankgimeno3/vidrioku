b51fe2ca55a676e5dfd561a2bb9819bbfb12adb8
frank gimeno <frankgimeno3@gmail.com>
2023-11-17 13:52:28 +0100

    inicio contenido solucitudes

--- src/app/missolicitudes/page.tsx
+++ src/app/missolicitudes/page.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function misSolicitudes() {
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/signin');
+    },
+  });
+  const [userId, setUserId] = useState("")
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (session?.data?.user?.email) {
+      setUserId(session.data.user.email);
+    } else { setUserId("Usuario") }
+  }, [session?.data?.user?.email]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true); // Indicar que se está cargando
+
+      const solicitudesCollection = collection(db, 'solicitudes');
+      const q = query(solicitudesCollection, where('empresa', '==', userId));
+      const querySnapshot = await getDocs(q);
+
+      const solicitudesData: Solicitud[] = [];
+      querySnapshot.forEach((doc) => {
+        solicitudesData.push(doc.data() as Solicitud);
+      });
+
+      setMisSolicitudes(solicitudesData);
+      setLoading(false); // Indicar que la carga ha finalizado
+    };
+
+    fetchData();
+  }, [userId]);
+
+
   return (
-    <div>check de mis solicitudes</div>
+    <div>
+      <h3>Solicitudes enviadas</h3>
+                {misSolicitudes.map((solicitud, index) => (
+            <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
+              <h3 className='font-medium'>{solicitud.titulo}</h3>
+              <p>{solicitud.cargo}</p>
+              <div className='flex flex-row justify-center pt-3'>
+              <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar solicitud</button>
+              <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver empresa</button>
+              </div>
+            </div>
+          ))}
+    </div>
   )
 }
 
