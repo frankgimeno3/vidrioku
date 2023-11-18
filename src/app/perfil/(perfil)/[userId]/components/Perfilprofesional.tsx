57f3eb3aa9eea349efb1b88b057bc0682c861c36
frank gimeno <frankgimeno3@gmail.com>
2023-11-18 03:04:42 +0100

    cambios en el perfil del profesional

--- src/app/perfil/(perfil)/[userId]/components/Perfilprofesional.tsx
+++ src/app/perfil/(perfil)/[userId]/components/Perfilprofesional.tsx
@@ -19,7 +19,7 @@ interface User {
   userEmail: string;
 }
 
-const Perfilprofesional: FC<PerfilprofesionalProps> = ({  }) => {
+const Perfilprofesional: FC<PerfilprofesionalProps> = ({ }) => {
   const session = useSession({
     required: true,
     onUnauthenticated() {
@@ -37,7 +37,7 @@ const Perfilprofesional: FC<PerfilprofesionalProps> = ({  }) => {
       setUserData('Usuario');
     }
   }, [session?.data?.user?.email]);
-  
+
   useEffect(() => {
     const fetchDoc = async () => {
       if (userData) {
@@ -57,29 +57,39 @@ const Perfilprofesional: FC<PerfilprofesionalProps> = ({  }) => {
   return (
     <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
       <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">  {user?.nombre}</h2>
-          <div className=" w-full flex justify-between bg-gradient-to-b from-slate-900 to-slate-600">
-            <div className="flex flex-col p-4 w-full flex justify-between text-center justify-center px--auto">
-              <Image src="/icons/empty-user-profile.png" alt="" width={200} height={200} className="mx-auto my-5" />
-              <div className="flex flex-row mx-auto">
-                <span className="mr-1">{user?.nombre}</span>
-                <span className="capitalize">{user?.apellidos}</span>
-              </div>
-              <div className="flex flex-row mx-auto">
-                <span className='mr-1'>{user?.edad} </span>
-                <span className="capitalize">({user?.genero})</span>
-              </div>
-              <span>{user?.ubi}</span>
-              <span>{userData}</span>
-        
-               <div >
-                <button
-                  className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
-                >Editar información de mi perfil de profesional</button>
-              </div>
-            </div>
+      <div className=" w-full flex justify-between bg-gradient-to-b from-slate-900 to-slate-600">
+        <div className="flex flex-col p-4 w-full flex justify-between text-center justify-center px--auto">
+          <Image src="/icons/empty-user-profile.png" alt="" width={200} height={200} className="mx-auto my-5" />
+          <div className="flex flex-row mx-auto">
+            <span className="mr-1">{user?.nombre}</span>
+            <span className="capitalize">{user?.apellidos}</span>
           </div>
-       <div className='mx-auto mt-5'>
-      <h2>Ofertas activas</h2> 
+          <div className="flex flex-row mx-auto">
+            <span className='mr-1'>{user?.edad} </span>
+            <span className="capitalize">({user?.genero})</span>
+          </div>
+          <span>{user?.ubi}</span>
+          <span>{userData}</span>
+
+          <div >
+            <button
+              className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
+            >Editar información de mi perfil de profesional</button>
+          </div>
+        </div>
+      </div>
+      <div className='mx-auto mt-5 text-center '>
+        <h2 className='font-bold'>Currículum vitae</h2>
+        <div className='flex flex-col my-5 items-center'>
+          <h3 className='mr-5'>Recorrido laboral</h3>
+          <button className='bg-white px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Editar</button>
+        </div>
+        <div className='flex flex-col my-5 items-center'>
+          <h3 className='mr-5'>Estudios, títulos, certificados y reconocimientos</h3>
+          <button className='bg-white px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Editar</button>
+
+        </div>
+        <button className='bg-white px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Descargar Currículum en PDF</button>
 
       </div>
     </div>
