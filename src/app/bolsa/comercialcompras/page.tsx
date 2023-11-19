f6e803d6427860a6d795f7cf87b500318621a99b
frank gimeno <frankgimeno3@gmail.com>
2023-11-19 12:02:29 +0100

    redirecciones funcionales desde bolsa

--- src/app/bolsa/comercialcompras/page.tsx
+++ src/app/bolsa/comercialcompras/page.tsx
@@ -1,10 +1,15 @@
+import NavUnlogged from '@/app/components/prelogged/NavUnlogged'
 import React from 'react'
 
 export default function comercialcompras
 () {
   return (
-    <div>comercialcompras
+    <>
+    <NavUnlogged   />
+    <div className='flex flex-col bg-gradient-to-b from-slate-950 to-slate-900 min-h-screen text-black  '>
+    comercialcompras
         
     </div>
+    </>
   )
 }
--- src/app/bolsa/operarios/page.tsx
+++ src/app/bolsa/operarios/page.tsx
@@ -1,8 +1,14 @@
+import NavUnlogged from '@/app/components/prelogged/NavUnlogged'
 import React from 'react'
 
 function operarios() {
   return (
-    <div>operarios</div>
+    <>
+    <NavUnlogged   />
+    <div className='flex flex-col bg-gradient-to-b from-slate-950 to-slate-900 min-h-screen text-black  '>
+      operarios
+      </div>
+      </>
   )
 }
 
