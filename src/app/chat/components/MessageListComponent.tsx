b8ab81c70057b92dea35b195c8daf0456501ac35
frank gimeno <frankgimeno3@gmail.com>
2023-11-27 20:19:08 +0100

    modifico chatlist para que consuma conversations

---
+++ src/app/chat/components/MessageListComponent.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import Image
+ from 'next/image'
+interface MessageListComponentProps {
+     selectUser1: any
+  
+}
+const MessageListComponent: FC<MessageListComponentProps> = ({ selectUser1,  }) => {
+
+   return (
+    <div className="flex  flex-row  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-1"
+    onClick={selectUser1}>
+    <div>
+      <Image
+        src="/profilepictures/2.jpg"
+        alt="ing1"
+        width={100}
+        height={100}
+        className=" shadow-lg rounded-full flex-1 mt-3 ml-3"
+      />
+    </div>
+
+    <div className='flex flex-col px-3 flex-3'>
+      <h2 className='text-right  pt-2 text-gray-400 text-sm'>Mensaje de usuario</h2>
+
+      <div className='flex flex-col'></div>
+      <h2 className='mt-1 text-sm   mx-5'>El usuario
+        <span className='font-bold'> Miquel Àngel Rodriguez </span>le ha enviado un mensaje </h2>
+    </div>
+  </div>
+  )
+}
+
+export default MessageListComponent
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -7,13 +7,8 @@ import { signOut, useSession } from 'next-auth/react';
 
 import ChatList from "./components/ChatList"
 import Chatcontent from "./components/Chatcontent"
-// interface NavbarProps {
-//   currentComponent: string;
-//   setCurrentComponent: (component: string) => void;
-// }
-
-// const Navbar: FC<NavbarProps> = ({ currentComponent, setCurrentComponent }) => {
-
+ 
+ 
 const Mensajes: FC = ({ }) => {
   const router = useRouter();
   const [userData, setUserData] = useState('');
@@ -36,33 +31,12 @@ const Mensajes: FC = ({ }) => {
       setUserData('Usuario');
     }
   }, [session?.data?.user?.email]);
-
-
+ 
 
   const selectUser1 = () => {
     setIsConversation(true)
     setUserSelectedName("Miquel Àngel Rodriguez")
     setUserSelectedImg("/profilepictures/3.jpg")
-  }
-  const selectUser2 = () => {
-    setIsConversation(true)
-    setUserSelectedName("TUROMAS")
-    setUserSelectedImg("/inventedlogos/4.png")
-  }
-  const selectUser3 = () => {
-    setIsConversation(true)
-    setUserSelectedName("Pepito Ramos")
-    setUserSelectedImg("/profilepictures/4.jpg")
-  }
-  const selectUser4 = () => {
-    setIsConversation(true)
-    setUserSelectedName("Tvitec")
-    setUserSelectedImg("/inventedlogos/5.png")
-  }
-  const selectUser5 = () => {
-    setIsConversation(true)
-    setUserSelectedName("GLASTON")
-    setUserSelectedImg("/inventedlogos/6.png")
   }
 
   const backToMenu = () => {
@@ -76,9 +50,10 @@ const Mensajes: FC = ({ }) => {
       <div className="flex flex-col  min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600 ">
         <h2 className="bg-zinc-800  bg-white bg-opacity-50 font-bold text-lg  py-3 text-center">Mensajes</h2>
         <div className='flex flex-row'>
-          <ChatList   userData={userData} selectUser1={selectUser1} selectUser2={selectUser2}
-            selectUser3={selectUser3} selectUser4={selectUser4} selectUser5={selectUser5} />
-          {isConversation && <Chatcontent userData={userData} backToMenu={backToMenu} userSelectedImg={userSelectedImg} userSelectedName={userSelectedName} />
+          <ChatList   userData={userData} selectUser1={selectUser1} />
+          {isConversation && 
+                <Chatcontent userData={userData} backToMenu={backToMenu} 
+                userSelectedImg={userSelectedImg} userSelectedName={userSelectedName} />
           }
         </div>
       </div>
