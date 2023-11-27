4a8b8fab54b4d73315bff074f499a7b8b00f6b87
frank gimeno <frankgimeno3@gmail.com>
2023-11-27 15:54:56 +0100

    redirige al chat sin que pete

--- src/app/solicitudes/(solicitudes)/[id]/components/ConectarButton.tsx
+++ src/app/solicitudes/(solicitudes)/[id]/components/ConectarButton.tsx
@@ -83,14 +83,12 @@ const ConectarButton: React.FC<ConectarButtonProps> = ({ usuario, oferta, solici
         addConversationInFirebase(solicitudId, usuario, userData)
     }
     return (
-        <>
-        {/* <Link href={"/chat"}> */}
+         <Link href={"/chat"}>
             <button className='bg-white px-4 py-2 rounded text-xs text-gray-500 shadow w-56 mx-auto my-2'
                 onClick={() => { startConversation() }}>
                 Conectar con el profesional</button>
-        {/* </Link> */}
-        </>
-    )
+        </Link>
+     )
 }
 
 export default ConectarButton
