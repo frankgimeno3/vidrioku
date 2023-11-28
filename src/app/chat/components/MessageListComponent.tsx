ebe79ce756cb04a10539a63eca8c9850ea8f650d
frank gimeno <frankgimeno3@gmail.com>
2023-11-28 13:39:47 +0100

    hago fetch de usuario a traves del fetch de conversacion

--- src/app/chat/components/MessageListComponent.tsx
+++ src/app/chat/components/MessageListComponent.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useEffect, useState } from 'react'
 import Image
- from 'next/image'
+  from 'next/image'
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/app/firebase';
 interface MessageListComponentProps {
-  value:any
+  value: any
 }
 interface User {
-  id:any
+  id: any
   apellidos: string;
   edad: number;
   genero: string;
@@ -16,44 +16,73 @@ interface User {
   userEmail: string;
   conversations: any
 }
+interface Conversation {
+  colaborador1: any
+  colaborador2: any
+  conversacion: any
+  conversationId: any
+  lastMessageSeenC1: any
+  lastMessageSeenc2: any
+  lastMessageSent: any
+  messagesArray: any
+}
 
-const MessageListComponent: FC<MessageListComponentProps> = ({ value}) => {
-  const [interlocutor, setInterlocutor] = useState<User>()
+const MessageListComponent: FC<MessageListComponentProps> = ({ value }) => {
+  const [interlocutor, setInterlocutor] = useState<any>()
+  const [conversationData, setConversationData] = useState<any>()
   useEffect(() => {
     const fetchDoc = async () => {
-        if (value) {
-            const docRef = doc(db, "users", value);
-            const response = await getDoc(docRef);
-            if (response.exists()) {
-                const interlocutorData = response.data() as User;
-                setInterlocutor(interlocutorData);
-            }
+      if (value) {
+        const docRef = doc(db, "conversations", value);
+        const response = await getDoc(docRef);
+        if (response.exists()) {
+          const conversationDataObject = response.data();
+          setConversationData(conversationDataObject);
+          console.log("conversationData", conversationData)
         }
+      }
     };
 
     fetchDoc();
-}, [value]);
+  }, [value]);
 
-   return (
+  useEffect(() => {
+    const fetchDoc = async () => {
+      if (value) {
+        const docRef = doc(db, "users", conversationData.colaborador2);
+        const response = await getDoc(docRef);
+        if (response.exists()) {
+          const interlocutorData = response.data() as User;
+          setInterlocutor(interlocutorData);
+          console.log("interlocutor", interlocutor)
+        }
+      }
+    };
+
+    fetchDoc();
+  }, [conversationData]);
+
+
+  return (
     <div className="flex  flex-row  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-1">
-    <div>
-      <Image
-        src="/profilepictures/2.jpg"
-        alt="ing1"
-        width={100}
-        height={100}
-        className=" shadow-lg rounded-full flex-1 mt-3 ml-3"
-      />
-    </div>
+      <div>
+        <Image
+          src="/profilepictures/2.jpg"
+          alt="ing1"
+          width={100}
+          height={100}
+          className=" shadow-lg rounded-full flex-1 mt-3 ml-3"
+        />
+      </div>
 
-    <div className='flex flex-col px-3 flex-3'>
-      <h2 className='text-right  pt-2 text-gray-400 text-sm'>{value}</h2>
+      <div className='flex flex-col px-3 flex-3'>
+        <h2 className='text-right  pt-2 text-gray-400 text-sm'>{value}</h2>
 
-      <div className='flex flex-col'></div>
-      <h2 className='mt-1 text-sm   mx-5'>El usuario
-        <span className='font-bold'> Miquel Àngel Rodriguez </span>le ha enviado un mensaje </h2>
+        <div className='flex flex-col'></div>
+        <h2 className='mt-1 text-sm   mx-5'>El usuario
+          <span className='font-bold'> {interlocutor?.nombre} {interlocutor?.apellidos} </span>le ha enviado un mensaje </h2>
+      </div>
     </div>
-  </div>
   )
 }
 
