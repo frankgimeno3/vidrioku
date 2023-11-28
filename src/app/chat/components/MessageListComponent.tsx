4bba0c2d06eed5aab9fb6feda31f3bd18592d3a2
frank gimeno <frankgimeno3@gmail.com>
2023-11-28 13:26:15 +0100

    renderizo interlocutorData

--- src/app/chat/components/MessageListComponent.tsx
+++ src/app/chat/components/MessageListComponent.tsx
@@ -1,10 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import Image
  from 'next/image'
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/app/firebase';
 interface MessageListComponentProps {
   value:any
 }
+interface User {
+  id:any
+  apellidos: string;
+  edad: number;
+  genero: string;
+  nombre: string;
+  ubi: string;
+  userEmail: string;
+  conversations: any
+}
+
 const MessageListComponent: FC<MessageListComponentProps> = ({ value}) => {
+  const [interlocutor, setInterlocutor] = useState<User>()
+  useEffect(() => {
+    const fetchDoc = async () => {
+        if (value) {
+            const docRef = doc(db, "users", value);
+            const response = await getDoc(docRef);
+            if (response.exists()) {
+                const interlocutorData = response.data() as User;
+                setInterlocutor(interlocutorData);
+            }
+        }
+    };
+
+    fetchDoc();
+}, [value]);
 
    return (
     <div className="flex  flex-row  mx-6 pb-3 bg-white bg-opacity-10  text-zinc-100  rounded-lg my-1">
