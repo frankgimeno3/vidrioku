580f38be07af04b35b2ac5093bfc48da8c9be10f
frank gimeno <frankgimeno3@gmail.com>
2023-12-14 22:05:53 +0100

    cambio estados ofertas home

--- src/app/components/prelogged/Ofertas.tsx
+++ src/app/components/prelogged/Ofertas.tsx
@@ -2,26 +2,58 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Ofertas: React.FC = ({ }) => {
- const router = useRouter()
+//  const router = useRouter()
+const [oferta1Height, setOferta1Height] = useState<any>({height: 145})
+const [t1Height, sett1Height] = useState<any>({ paddingTop: '15px' });
+const [oferta2Height, setOferta2Height] = useState<any>({height: 145})
+const [t2Height, sett2Height] = useState<any>({ paddingTop: '35px' });
+const [oferta3Height, setOferta3Height] = useState<any>({height: 145})
+const [t3Height, sett3Height] = useState<any>({ paddingTop: '35px' });
+const [oferta4Height, setOferta4Height] = useState<any>({height: 145})
+const [t4Height, sett4Height] = useState<any>({ paddingTop: '20px' });
 
+const [is1, setIs1] = useState(false)
+ const [is2, setIs2] = useState(false)
+ const [is3, setIs3] = useState(false)
+ const [is4, setIs4] = useState(false)
  const handleDepTecnico = ()=>{
   // router.push("/bolsaofertas/deptecnico")
+  if(`${is1}` == `true`) {setIs1(false); setOferta1Height({height: 145}); sett1Height({ paddingTop: '15px' })}
+  if(`${is1}` == `false`) {setIs1(true); setOferta1Height({height: 180}); sett1Height({ paddingTop: '1px' })}
+  setIs2(false); setIs3(false); setIs4(false)
+  setOferta2Height({height: 145}); setOferta3Height({height: 145}); setOferta4Height({height: 145})
+  sett2Height({ paddingTop: '35px' }); sett3Height({ paddingTop: '35px' }); sett4Height({ paddingTop: '20px' })
  }
  const handleIngenieros
  = ()=>{
   // router.push("/bolsaofertas/ingenieros")
+  if(`${is2}` == `true`) {setIs2(false); setOferta2Height({height: 145}); sett2Height({ paddingTop: '35px' })}
+  if(`${is2}` == `false`) {setIs2(true); setOferta2Height({height: 180}); sett2Height({ paddingTop: '5px', marginBottom: '15px'})}
+  setIs1(false); setIs3(false); setIs4(false)
+  setOferta1Height({height: 145}); setOferta3Height({height: 145}); setOferta4Height({height: 145})
+  sett1Height({ paddingTop: '15px' }); sett3Height({ paddingTop: '35px' }); sett4Height({ paddingTop: '20px' })
  }
  const  handleOperarios
   = ()=>{
   // router.push("/bolsaofertas/operarios")
+  if(`${is3}` == `true`) {setIs3(false) ; setOferta3Height({height: 145}); sett3Height({ paddingTop: '35px' })}
+  if(`${is3}` == `false`) {setIs3(true); setOferta3Height({height: 180}); sett3Height({ paddingTop: '5px', marginBottom: '5px'})}
+  setIs1(false); setIs2(false); setIs4(false)
+  setOferta1Height({height: 145}); setOferta2Height({height: 145}); setOferta4Height({height: 145})
+  sett1Height({ paddingTop: '15px' }); sett2Height({ paddingTop: '35px' }); sett4Height({ paddingTop: '20px' })
  }
  const handleComercialCompras = ()=>{
   // router.push("/bolsaofertas/comercialcompras")
+  if(`${is4}` == `true`) {setIs4(false) ; setOferta4Height({height: 145}); sett4Height({ paddingTop: '20px' })}
+  if(`${is4}` == `false`) {setIs4(true); setOferta4Height({height: 180}); sett4Height({ paddingTop: '0px' })}
+  setIs1(false); setIs2(false); setIs3(false)
+  setOferta1Height({height: 145}); setOferta2Height({height: 145}); setOferta3Height({height: 145})
+  sett1Height({ paddingTop: '15px' }); sett2Height({ paddingTop: '35px' }); sett3Height({ paddingTop: '35px' })
  }
 
   return (
@@ -35,7 +67,7 @@ const Ofertas: React.FC = ({ }) => {
           <p className="mb-2">
             Arquitectos técnicos e ingenieros, especializados en la realización y el análisis de proyectos en que el vidrio juega un factor clave
           </p>
-          <Image src="/icons/dtecnico.png" alt="Logo3" width={200} height={200}  className='opacity-60 mx-auto hover:opacity-100'/>
+          {/* <Image src="/icons/dtecnico.png" alt="Logo3" width={200} height={200}  className='opacity-60 mx-auto hover:opacity-100'/> */}
 
         </div>
         <div className="bg-white rounded shadow p-2 md:p-5 px-10 my-2 mx-5 h-96"  onClick={handleIngenieros}      >
@@ -45,7 +77,7 @@ const Ofertas: React.FC = ({ }) => {
           <p className="mb-2">
             En nuestra bolsa específica de ingenieros encontrará personal licenciado en ingeniería, filtrado según su educación o experiencia previa
           </p>
-          <Image src="/icons/ingenieros.png" alt="Logo3" width={200} height={200}  className='opacity-60 mx-auto hover:opacity-100'/>
+          {/* <Image src="/icons/ingenieros.png" alt="Logo3" width={200} height={200}  className='opacity-60 mx-auto hover:opacity-100'/> */}
 
         </div>
       </div>
@@ -68,7 +100,7 @@ const Ofertas: React.FC = ({ }) => {
           <p className="mb-2">
             Arquitectos técnicos e ingenieros, especializados en la realización y el análisis de proyectos en que el vidrio juega un factor clave
           </p>
-          <Image src="/icons/comercialycompras.png" alt="Logo3" width={150} height={150}  className='opacity-60 mx-auto hover:opacity-100'/>
+          {/* <Image src="/icons/comercialycompras.png" alt="Logo3" width={150} height={150}  className='opacity-60 mx-auto hover:opacity-100'/> */}
 
         </div>
       </div>
