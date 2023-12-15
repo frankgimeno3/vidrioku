30bc305601606111a25705ba27d1b03fb2fa4ffe
frank gimeno <frankgimeno3@gmail.com>
2023-12-15 14:56:15 +0100

    dashboard usuario mejorado

--- src/app/dashboard/components/HomeTrab.tsx
+++ src/app/dashboard/components/HomeTrab.tsx
@@ -28,9 +28,6 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
   const router = useRouter();
   const [user, setUser] = useState<User>();
 
-  const handlemissolicitudes = () => {
-    router.push("/missolicitudes")
-  }
   useEffect(() => {
     const fetchDoc = async () => {
       if (userData) {
@@ -50,39 +47,37 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
     router.push(`/perfil/${userData}`)
   }
 
+
   return (
     <div className='flex flex-col'>
-      <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">Saludos, {user?.nombre}</h2>
-      <div className="flex flex-row   bg-gradient-to-b from-zinc-900 to-zinc-600 ml-6">
-        <div className='flex flex-row h-full  ' style={{ height: '750px', width: '650px' }}           >
+      <div className="flex flex-row w-full justify-between  bg-gradient-to-b from-zinc-900 to-zinc-600 " style={{ height: '800px' }} >
+        <div className='flex flex-row h-full  ' style={{ height: '800px', width: '650px' }}           >
           <div className=" w-full flex  h-full ">
-            <div className="flex flex-col   flex  text-center justify-center w-full  bg-white text-gray-500    shadow-xl">
-              
-              <div className='bg-gradient-to-b from-blue-500 to-zinc-600 h-full px-12 flex flex-col pt-24 pb-12 flex-1'>
-              <div className='flex flex-col'>
-                <Image src={user?.profilepicture || "/icons/empty-user-profile.png"} alt="" width={200} height={200} className="rounded-full mx-auto my-5 shadow-xl" />
-                <div className='flex flex-col my-auto   justify-left text-center text-white'>
-                  <p className="flex flex-row flex-wrap font-medium text-lg mx-auto">
-                    <span className="mr-1">{user?.nombre}</span>
-                    <span className="capitalize">{user?.apellidos}</span>
-                  </p>
-                  <span>{user?.ubi}</span>
-                  <span className='italic'>{userData}</span>
+            <div className="flex flex-col   flex  text-center justify-center w-full  bg-white text-gray-500  ">
+
+              <div className='bg-gradient-to-b from-cyan-600 to-zinc-700 h-full px-12 flex flex-col pt-24 pb-12 flex-1'>
+                <div className='flex flex-col'>
+                  <Image src={user?.profilepicture || "/icons/empty-user-profile.png"} alt="" width={200} height={200} className="rounded-full mx-auto my-5 shadow-xl" />
+                  <div className='flex flex-col my-auto   justify-left text-center text-white'>
+                    <p className="flex flex-row flex-wrap font-medium text-lg mx-auto">
+                      <span className="mr-1">{user?.nombre}</span>
+                      <span className="capitalize">{user?.apellidos}</span>
+                    </p>
+                    <span>{user?.ubi}</span>
+                    <span className='italic'>{userData}</span>
+                  </div>
                 </div>
-              </div>
-              <div >
                 <button
                   className="bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-5 text-sm m-1"
                   onClick={miPerfilHandler}
                 >Perfil Completo</button>
               </div>
-              </div>
               <div className='flex flex-col flex-1 pt-12 px-12'>
-              <button
+                <button
                   className="bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-5 text-sm m-1"
                   onClick={miPerfilHandler}
                 >Configuración de la cuenta</button>
-                 <button
+                <button
                   className="bg-gray-200 hover:bg-gray-400 shadow-lg border text-gray-700 border-gray-200 rounded px-4 py-2 mt-5 text-sm m-1"
                   onClick={miPerfilHandler}
                 >Cerrar Sesión</button>
@@ -90,41 +85,57 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
             </div>
           </div>
         </div>
-        <div className='flex flex-col text-white h-full w-full m-12 border border-red-300'  >
-          <div className='flex flex-col w-full h-full  '>
-            <div className='h-full flex-1 text-center w-full  flex-col bg-white  w-full  p-2   text-gray-900'>
-              <p className='font-medium '>Mensajes nuevos</p>
-              <p>Tienes <span className='font-bold mx-1'>X</span> mensajes nuevos</p>
-              <Link href={'/chat'}>
+        <div className='flex flex-col   text-white h-full w-full    text-white text-2xl font-medium'  >
+          <h2 className="bg-zinc-800 bg-gray-600 font-bold text-lg py-3 text-center">Saludos, {user?.nombre}</h2>
+          <div className='flex flex-col mx-12 h-full pt-12'>
+            <p className=''> Mensajes</p>
+            <div className='bg-white rounded-lg shadow shadow-xl my-4 text-center '>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> Has recibido x mensajes nuevos</p>
                 <button
-                  className="bg-white hover:bg-gray-50 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-3 text-sm m-1"
-                >Mis chats</button>
-              </Link>
-            </div>
-
-            <div className='h-full flex-1 text-center flex-col bg-white   p-2   text-gray-900'>
-
-              <p className='font-medium'>Solicitudes enviadas</p>
-              <p><span className='font-bold mx-1'>X</span> solicitudes enviadas</p>
-              <button
-                className="bg-white hover:bg-gray-50 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-3 text-sm m-1"
-                onClick={handlemissolicitudes}
-              >Mis solicitudes</button>
-            </div>
-            <div className='h-full flex-1 text-center '>
-              <div className='fex flex-col bg-white    p-2   text-gray-900'>
-
-                <p className='font-medium'>Nuevos contactos</p>
-                <p><span className='font-bold mx-1'>X</span> nuevas solicitudes aceptadas</p>
-                <p> <span className='font-bold mx-1'>X</span> nuevos contactos de empresas</p>
+                  className=" bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mb-6 mt-2 text-base font-light "
+                  onClick={() => { router.push("/chat") }}
+                >Mis mensajes</button>
               </div>
             </div>
-
+            <p className='pt-8'> Solicitudes</p>
+            <div className='bg-white rounded-lg shadow shadow-xl my-4 text-center '>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> Has enviado x solicitudes</p>
+                <button
+                  className=" bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mb-6 mt-2 text-base font-light "
+                  onClick={() => { router.push("/missolicitudes") }}
+                >Mis solicitudes</button>
+              </div>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> X empresas han aceptado tus solicitudes</p>
+                <button
+                  className=" bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mb-6 mt-2 text-base font-light "
+                  onClick={() => { router.push("/chat") }}
+                >Ir al chat</button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col' style={{ width: '200px' }}>
-          <Image src={'/inventedlogos/banner.jpg'} alt={''} width={250} height={100} />
-          <Image src={'/inventedlogos/banner.jpg'} alt={''} width={250} height={100} />
+        <div className='flex flex-col border border-red-300 overflow-hidden bg-white h-[800px]'>
+          <div className='h-1/2'>
+            <Image
+              src={'/inventedlogos/banner.jpg'}
+              alt={''}
+              width={400} // Ajusta este valor según sea necesario
+              height={400}
+              objectFit="cover"
+            />
+          </div>
+          <div className='h-1/2'>
+            <Image
+              src={'/inventedlogos/banner.jpg'}
+              alt={''}
+              width={400} // Ajusta este valor según sea necesario
+              height={400}
+              objectFit="cover"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -133,7 +144,3 @@ const HomeTrab: FC<HomeTrabProps> = ({ userData }) => {
 };
 
 export default HomeTrab;
-
-function setUserData(email: string) {
-  throw new Error('Function not implemented.');
-}
