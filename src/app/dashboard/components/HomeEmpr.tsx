2e274995532c44fbd00e2a63a7af868fe4fe600b
frank gimeno <frankgimeno3@gmail.com>
2023-12-15 15:19:19 +0100

    reajusto el dashboard de empresa en base al de usuario

--- src/app/dashboard/components/HomeEmpr.tsx
+++ src/app/dashboard/components/HomeEmpr.tsx
@@ -16,6 +16,7 @@ interface User {
   nombre: string;
   ubi: string;
   userEmail: string;
+  profilepicture:any;
 }
 
 const HomeEmpr: FC<HomeEmprProps> = ({ userData }) => {
@@ -36,10 +37,7 @@ const HomeEmpr: FC<HomeEmprProps> = ({ userData }) => {
 
     fetchDoc();
   }, [userData]);
-
-  const chathandler = () => {
-    router.push("/chat")
-  }
+ 
   const perfilhandler = () => {
     router.push("/perfil")
   }
@@ -50,74 +48,116 @@ const HomeEmpr: FC<HomeEmprProps> = ({ userData }) => {
   const misofertashandler = () => {
     router.push("/misofertas")
   }
-  const missolicitudeshandler = () => {
-    router.push("/solicitudes")
+
+  const miPerfilHandler = () => {
+    router.push(`/perfil/${userData}`)
   }
 
+
   return (
-    <div className="flex flex-col  justify-between h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
-      <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">Saludos, {user?.nombre}</h2>
+    <div className='flex flex-col'>
+      <div className="flex flex-row w-full justify-between  bg-gradient-to-b from-zinc-900 to-zinc-600 " style={{ height: '800px' }} >
+        <div className='flex flex-row h-full  ' style={{ height: '800px', width: '650px' }}           >
+          <div className=" w-full flex  h-full ">
+            <div className="flex flex-col   flex  text-center justify-center w-full  bg-white text-gray-500  ">
 
-      <div className=" w-full flex justify-between bg-gradient-to-b from-slate-900 to-slate-600">
-        <div className="flex flex-col p-4 w-full flex justify-between text-center justify-center px--auto">
-          <Image src="/icons/empresas.png" alt="" width={200} height={200} className="mx-auto my-5" />
-          <div className="flex flex-row mx-auto">
-            <span className="mr-1">{user?.nombre}</span>
-            <span className="capitalize">{user?.apellidos}</span>
+
+              <div className='bg-gradient-to-b from-cyan-600 to-zinc-700 h-full px-12 flex flex-col pt-24 pb-12 flex-1'>
+                <div className='flex flex-col'>
+                  <Image src={user?.profilepicture || "/icons/empresas.png"} alt="" width={200} height={200} className="rounded-full mx-auto my-5 shadow-xl" />
+                  <div className='flex flex-col my-auto   justify-left text-center text-white'>
+                    <p className="flex flex-row flex-wrap font-medium text-lg mx-auto">
+                      <span className="mr-1">{user?.nombre}</span>
+                      <span className="capitalize">{user?.apellidos}</span>
+                    </p>
+                    <span>{user?.ubi}</span>
+                    <span className='italic'>{userData}</span>
+                  </div>
+                </div>
+                <button
+                  className="bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-5 text-sm m-1"
+                  onClick={miPerfilHandler}
+                >Perfil Completo</button>
+              </div>
+              <div className='flex flex-col flex-1 pt-12 px-12'>
+                <button
+                  className="bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mt-5 text-sm m-1"
+                  onClick={miPerfilHandler}
+                >Configuración de la cuenta</button>
+                <button
+                  className="bg-gray-200 hover:bg-gray-400 shadow-lg border text-gray-700 border-gray-200 rounded px-4 py-2 mt-5 text-sm m-1"
+                  onClick={miPerfilHandler}
+                >Cerrar Sesión</button>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-row mx-auto">
-            <span className='mr-1'>{user?.edad} </span>
-            <span className="capitalize">({user?.genero})</span>
+        </div>
+        <div className='flex flex-col   text-white h-full w-full    text-white text-2xl font-medium'  >
+        <h2 className="bg-zinc-800 bg-gray-600 font-bold text-lg py-3 text-center">Saludos, {user?.nombre}</h2>
+          <div className='flex flex-col mx-12 h-full pt-4'>
+            <p className=''> Mensajes</p>
+            <div className='bg-white rounded-lg shadow shadow-xl mb-3 mt-2 text-center '>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> Has recibido x mensajes nuevos</p>
+                <button
+                  className=" bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mb-6 mt-2 text-base font-light "
+                  onClick={() => { router.push("/chat") }}
+                >Mis mensajes</button>
+              </div>
+            </div>
+            <p className='pt-4'> Mis ofertas</p>
+            <div className='flex flex-col bg-white rounded-lg shadow shadow-xl mb-3 mt-2 text-center '>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> Tienes X ofertas publicadas</p>
+              <div className='flex flex-row  justify-center  font-light mb-6'>
+                <button
+                  className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-sm m-1  "
+                  onClick={crearofertahandler}
+                >Crear oferta de empleo</button>
+                <button
+                  className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-sm m-1"
+                  onClick={misofertashandler}
+                >Mis Ofertas</button>
+              </div>
+              </div>
+
+            </div>
+            <p className='pt-4'> Solicitudes</p>
+            <div className='bg-white rounded-lg shadow shadow-xl mb-3 mt-2 text-center '>
+              <div className='shadow shadow-lg border border-gray-100 border-sm m-6  rounded-lg'>
+                <p className='text-center font-light text-gray-500 text-lg px-12 pt-6'> Has recibido x solicitudes</p>
+                <button
+                  className=" bg-white hover:bg-gray-100 shadow-lg border text-gray-500 border-gray-100 rounded px-4 py-2 mb-6 mt-2 text-base font-light "
+                  onClick={() => { router.push("/solicitudes") }}
+                >Mis solicitudes</button>
+              </div>
+
+            </div>
           </div>
-          <span>{user?.ubi}</span>
-          <span>{userData}</span>
-          <div >
-            <button
-              onClick={perfilhandler}
-              className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
-            >Perfil de empresa completo</button>
+        </div>
+        <div className='flex flex-col border border-red-300 overflow-hidden bg-white h-[800px]'>
+          <div className='h-1/2'>
+            <Image
+              src={'/inventedlogos/banner.jpg'}
+              alt={''}
+              width={400} // Ajusta este valor según sea necesario
+              height={400}
+              objectFit="cover"
+            />
           </div>
-
-        </div>
-      </div>
-
-      <h2 className="bg-zinc-800 bg-white bg-opacity-50 font-bold text-lg py-3 text-center">Qué te has perdido?</h2>
-      <div className='flex flex-row bg-gradient-to-b from-slate-900 to-slate-600 h-full'>
-        <div className='h-full flex-1 text-center my-3'>
-          <p className='font-bold'>Mensajes nuevos</p>
-          <button
-            className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1"
-            onClick={chathandler}
-          >Ver mensajes nuevos</button>
-        </div>
-        <div className='flex h-full flex-1 flex-col text-center my-3'>
-          <p className='font-bold'>Mis ofertas de empleo</p>
-          <button
-            className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1 mx-32"
-            onClick={crearofertahandler}
-          >Crear oferta de empleo</button>
-          <button
-            className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1 mx-32"
-            onClick={misofertashandler}
-          >Mis Ofertas</button>
-
-        </div>
-
-        <div className='h-full flex-1 text-center my-3'>
-          <p className='font-bold'>Solicitudes  </p>
-          <button
-            className="bg-white shadow border text-gray-500 border-gray-200 rounded px-4 py-2 text-xs m-1 mx-32"
-            onClick={missolicitudeshandler}
-          >Ver Solicitudes Recibidas</button>
-
-        </div>
-        <div className='h-full flex-1 text-center my-3'>
-          <p className='font-bold'>Seguimientos</p>
-          <p>Cambios en profesionales seguidos</p>
-          <p>Cambios en empresas seguidas</p>
+          <div className='h-1/2'>
+            <Image
+              src={'/inventedlogos/banner.jpg'}
+              alt={''}
+              width={400} // Ajusta este valor según sea necesario
+              height={400}
+              objectFit="cover"
+            />
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
