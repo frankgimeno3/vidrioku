0aca3b7c94168a0cff3c09098fc189ddda43ab21
frank gimeno <frankgimeno3@gmail.com>
2023-12-20 11:29:29 +0100

    remaqueto misofertas

--- src/app/misofertas/page.tsx
+++ src/app/misofertas/page.tsx
@@ -7,6 +7,7 @@ import { collection, doc, getDoc, getDocs, query, setDoc, where } from 'firebase
 import { db } from '../firebase';
 import Link from 'next/link';
 import Footer from '../components/Footer';
+import Banners from '../components/Banners';
 
 type Oferta = {
   id: string;
@@ -72,65 +73,73 @@ const Misofertas: FC = () => {
   return (
     <>
       <Navbar />
+      <div className='flex flex-row w-full h-full justify-between bg-white bg-opacity-90'>
 
-      <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600">
-        <div className='flex flex-row justify-between py-3 bg-zinc-800 bg-opacity-50 px-60'>
-          <h2 className="   font-bold text-lg   ">Mis Ofertas</h2>
-          <button className='bg-white rounded-lg px-3 py-1 text-sm text-gray-500'
-            onClick={nuevaofertahandler}>Crear nueva oferta</button>
-        </div>
-        <div className="p-5 bg-white bg-opacity-10 ">
-          <h2 className='ml-56'>Ofertas activas</h2>
-          {ofertasActivas.length > 0 ? (
-            ofertasActivas.map((oferta, index) => (
-              <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
-                <h3 className='font-medium'>{oferta.titulo}</h3>
-                <p>{oferta.cargo}</p>
-                <p>Id de la oferta: {oferta.id}</p>
-                <div className='flex flex-row justify-center pt-3'>
-                  <Link href={`/misofertas/editar/${oferta.id}`}>
-                    <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Editar oferta</button>
-                  </Link>
-                  <Link href={`/solicitudes/${oferta.id}`}>
-                    <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes de la oferta</button>
-                  </Link>
-                  <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Desactivar oferta</button>
-                  <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg '>
-              <p className='p-12'>No hay ofertas activas</p>
-            </div>)}
-        </div>
-
-        <div className="p-5 bg-white bg-opacity-10 ">
-          <h2 className='ml-56'>Ofertas inactivas</h2>
-          {ofertasInactivas.length > 0 ? (
-            ofertasInactivas.map((oferta, index) => (
-              <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
-                <h3 className='font-medium'>{oferta.titulo}</h3>
-                <p>{oferta.cargo}</p>
-                <div className='flex flex-row justify-center pt-3'>
-                  <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Reactivar oferta</button>
-                  <Link href={`/misofertas`}>
-                    <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes de la oferta</button>
-                  </Link>
-                  <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
-
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg '>
-              <p className='p-12'>No hay ofertas inactivas</p>
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-600 w-full">
+          <div className='flex flex-row justify-between py-3 bg-zinc-800 bg-opacity-50 px-60'>
+            <h2 className="   font-bold text-lg   ">Mis Ofertas</h2>
+          </div>
+          <div className="p-5 bg-white bg-opacity-10 ">
+            <div className='flex flex-row justify-between mx-56 '>
+              <h2 className=''>Ofertas activas</h2>
+              <button className='bg-white rounded-lg px-3 py-1 text-sm text-gray-500'
+                onClick={nuevaofertahandler}>Crear nueva oferta</button>
             </div>
+            {ofertasActivas.length > 0 ? (
+              ofertasActivas.map((oferta, index) => (
+                <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
+                  <h3 className='font-medium'>{oferta.titulo}</h3>
+                  <p>{oferta.cargo}</p>
+                  <p>Id de la oferta: {oferta.id}</p>
+                  <div className='flex flex-row justify-center pt-3'>
+                    <Link href={`/misofertas/editar/${oferta.id}`}>
+                      <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Editar oferta</button>
+                    </Link>
+                    <Link href={`/solicitudes/${oferta.id}`}>
+                      <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes de la oferta</button>
+                    </Link>
+                    <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Desactivar oferta</button>
+                    <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg '>
+                <p className='p-12'>No hay ofertas activas</p>
+              </div>)}
+          </div>
 
-          )}
+          <div className="p-5 bg-white bg-opacity-10 ">
+            <h2 className='ml-56'>Ofertas inactivas</h2>
+            {ofertasInactivas.length > 0 ? (
+              ofertasInactivas.map((oferta, index) => (
+                <div key={index} className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg'>
+                  <h3 className='font-medium'>{oferta.titulo}</h3>
+                  <p>{oferta.cargo}</p>
+                  <div className='flex flex-row justify-center pt-3'>
+                    <button className='shadow px-2 h-8 mr-2 bg-gray-50 text-sm rounded-lg'>Reactivar oferta</button>
+                    <Link href={`/misofertas`}>
+                      <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Ver solicitudes de la oferta</button>
+                    </Link>
+                    <button className='shadow px-2 h-8 ml-2 bg-gray-50 text-sm rounded-lg'>Eliminar oferta</button>
+
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className='my-2 bg-white text-gray-800 p-3 mx-56 text-center rounded-lg '>
+                <p className='p-12'>No hay ofertas inactivas</p>
+              </div>
+
+            )}
+          </div>
+        </div>
+
+        <div className='h-full bg-white bg-opacity-5'>
+          <Banners widthProp={250} />
         </div>
       </div>
-      <Footer  />
+      <Footer />
 
     </>
   );
