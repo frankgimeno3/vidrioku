459f77e79a3f8241bd99f78505e2eee5b033906e
frank gimeno <frankgimeno3@gmail.com>
2024-01-03 23:31:39 +0100

    aplico changeReadState a chats sin ID

--- src/app/chat/components/MessageListComponent.tsx
+++ src/app/chat/components/MessageListComponent.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
 import Image  from 'next/image'
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { db } from '@/app/firebase';
 import { useRouter } from 'next/navigation';
  
@@ -48,6 +48,7 @@ const MessageListComponent: FC<MessageListComponentProps> = ({ conversation, use
   const [contenidoUltimo, setContenidoUltimo] = useState<any>()
   const [conversationId, setConversationId] = useState<any>()
   const [interlocutorId, setInterlocutorId] = useState<any>()
+  const [quienSomos, setQuienSomos] = useState<any>()
   const [isMessageSeen, setIsMessageSeen] = useState<any>(true)
   const [background, setBackground] = useState('white');
 
@@ -74,13 +75,15 @@ const MessageListComponent: FC<MessageListComponentProps> = ({ conversation, use
       if (conversationData) {
         if (conversationData.colaborador2 == user.id) {
           setInterlocutorId(conversationData.colaborador1)
+          setQuienSomos('colaborador2')
           if (conversationData.lastMessageSeenc2 == false) { setIsMessageSeen(false) }
-          else{setIsMessageSeen(true)}
+          else { setIsMessageSeen(true) }
         }
         if (conversationData.colaborador2 != user.id) {
           setInterlocutorId(conversationData.colaborador2)
+          setQuienSomos('colaborador1')
           if (conversationData.lastMessageSeenc1 == false) { setIsMessageSeen(false) }
-          else{setIsMessageSeen(true)}
+          else { setIsMessageSeen(true) }
         }
       }
     };
@@ -89,12 +92,13 @@ const MessageListComponent: FC<MessageListComponentProps> = ({ conversation, use
   }, [conversationData]);
 
   useEffect(() => {
-     if (isMessageSeen) {
-      setBackground('bg-sky-500 bg-opacity-50 hover:bg-opacity-60'); 
+    if (isMessageSeen) {
+      setBackground('bg-white bg-opacity-10 hover:bg-opacity-20');
     } else {
-      setBackground('bg-white bg-opacity-10 hover:bg-opacity-20');  
+      setBackground('bg-sky-500 bg-opacity-50 hover:bg-opacity-60');
     }
   }, [isMessageSeen]);
+
 
   useEffect(() => {
     const fetchDoc = async () => {
@@ -143,10 +147,62 @@ const MessageListComponent: FC<MessageListComponentProps> = ({ conversation, use
     fetchDoc();
   }, [lastMessage]);
 
+  const changeReadState = async (lastMessageSeenC1: any, lastMessageSeenc2: any, quienSomos: any) => {
+    try {
+      const docRef = doc(db, "conversations", conversationId);
+      const userDoc = await getDoc(docRef);
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+
+
+        if (quienSomos == 'colaborador1') {
+          const updatedData = {
+            lastMessageSeenC1: true
+          };
+
+          const filteredData = Object.fromEntries(
+            Object.entries(updatedData).filter(([_, value]) => value !== undefined)
+          );
+
+          await setDoc(docRef, {
+            ...userData,
+            ...filteredData,
+          });
+        }
+        if (quienSomos == 'colaborador2') {
+          const updatedData = {
+            lastMessageSeenc2: true
+          };
+
+          const filteredData = Object.fromEntries(
+            Object.entries(updatedData).filter(([_, value]) => value !== undefined)
+          );
+
+          await setDoc(docRef, {
+            ...userData,
+            ...filteredData,
+          });
+        }
+      } else {
+        console.error('El documento de la conversación');
+      }
+    } catch (error) {
+      console.error('Error al crear marcar mensaje como leído:', error);
+    }
+  };
+
+
+  const clickOnChat = () => {
+    router.push(`/chat/${conversationId}`)
+    changeReadState(conversationData.lastMessageSeenC1, conversationData.lastMessageSeenc2, quienSomos)
+  }
+
+
   return (
     <div
-      className={`flex flex-row mx-6 pb-3 ${background}   text-zinc-100 rounded-lg my-1`}
-        onClick={() => { router.push(`/chat/${conversationId}`) }}>
+    className={`flex flex-row mx-6 pb-3 ${background}   text-zinc-100 rounded-lg my-1`}
+    onClick={clickOnChat}>
      <div>
         <Image
           src={interlocutor?.profilepicture || "/icons/empty-user-profile.png"}
