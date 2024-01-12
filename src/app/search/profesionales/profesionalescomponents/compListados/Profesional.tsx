a920f245bbb4e8f1cb339b2b9f8b5402fd964160
frank gimeno <frankgimeno3@gmail.com>
2024-01-12 02:16:27 +0100

    reajusto perfiles de profesional en listado respecto ofertas

--- src/app/search/profesionales/profesionalescomponents/compListados/Profesional.tsx
+++ src/app/search/profesionales/profesionalescomponents/compListados/Profesional.tsx
@@ -2,15 +2,23 @@ import { FC } from 'react';
 import Image from 'next/image';
 
 interface profesionalProps {
-  trabajador:any;
-  setRenderProfesional:any;
+  trabajador: any;
+  setRenderProfesional: any;
 }
 
-const profesional: FC <profesionalProps> = ({trabajador, setRenderProfesional}) => {
+const profesional: FC<profesionalProps> = ({ trabajador, setRenderProfesional }) => {
   return (
     <div className="flex flex-row justify-left items-center p-5 bg-gray-50 hover:bg-gray-200 shadow-lg mb-1 text-gray-600"
-    onClick={()=>{setRenderProfesional(trabajador)}}>
-      <Image src={trabajador?.profilepicture || "/icons/empty-user-profile.png"} alt="pepo" height={75} width={75}  />
+      onClick={() => { setRenderProfesional(trabajador) }}>
+<div className='w-24 h-20 overflow-hidden'>
+  <Image
+    src={trabajador?.profilepicture || "/icons/empty-user-profile.png"}
+    alt="Profesional"
+    width={100}
+    height={100}
+    style={{ objectFit: 'cover', height: '100%', width: 'auto' }}
+  />
+</div>
       <div className='justify-left pl-5 w-full'>
         <h2>{trabajador?.nombre} {trabajador?.apellidos}</h2>
         <div className='flex flex-row justify-between w-full'>
