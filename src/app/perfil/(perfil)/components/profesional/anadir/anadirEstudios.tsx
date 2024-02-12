a84b8489bc56d484306a798c17b3f251f658f8de
frank gimeno <frankgimeno3@gmail.com>
2024-02-12 16:00:32 +0100

    fondo en negro clickable en pestañas añadir

--- src/app/perfil/(perfil)/components/profesional/anadir/anadirEstudios.tsx
+++ src/app/perfil/(perfil)/components/profesional/anadir/anadirEstudios.tsx
@@ -20,39 +20,40 @@ const AnadirEstudios: FC<anadirEstudiosProps> = ({ setIsEstudiosSelected }) => {
 
     return (
         <>
-        <div className='h-screen w-screen' onClick={()=>{setIsEstudiosSelected(false)}}>
-        </div>
-        <div className='flex flex-col bg-white text-gray-500 px-5  mx-auto mt-36 pb-16 border border-gray-50 shadow-xl rounded-md' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            <div className="flex justify-end py-6 pr-2 ">
-                <svg className="h-8 w-8 cursor-pointer text-gray-300 hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" 
-                onClick={()=>{setIsEstudiosSelected(false)}}
-                >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
+            <div className='w-screen bg-black bg-opacity-70' style={{ height: '2500px' }}
+                onClick={() => { setIsEstudiosSelected(false) }}>
             </div>
-            <div className='px-20'>
-                <p className='text-xl font-medium pb-5'>Añadir estudios, títulos, certificados o reconocimientos</p>
-                <form className='flex flex-col px-5'>
-                    <label> Concepto</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                     placeholder='Grado superior en electromecánica' />
-                    <label> Descripción</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                     placeholder='(opcional)' />
-                    <label> Desde</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                     placeholder='Año de inicio' />
-                    <label> Hasta</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                     placeholder='Año de finalización' />
-                    <label> Entidad emisora</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                     placeholder='UPC - Universitat Politecnica Catalunya' />
-          
-                <button className='bg-gray-100 hover:bg-gray-200 px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Añadir</button>
-                </form>
+            <div className='flex flex-col bg-white text-gray-500 px-5  mx-auto mt-36 pb-16 border border-gray-50 shadow-xl rounded-md' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                <div className="flex justify-end py-6 pr-2 ">
+                    <svg className="h-8 w-8 cursor-pointer text-gray-300 hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"
+                        onClick={() => { setIsEstudiosSelected(false) }}
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </div>
+                <div className='px-20'>
+                    <p className='text-xl font-medium pb-5'>Añadir estudios, títulos, certificados o reconocimientos</p>
+                    <form className='flex flex-col px-5'>
+                        <label> Concepto</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Grado superior en electromecánica' />
+                        <label> Descripción</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='(opcional)' />
+                        <label> Desde</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Año de inicio' />
+                        <label> Hasta</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Año de finalización' />
+                        <label> Entidad emisora</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='UPC - Universitat Politecnica Catalunya' />
+
+                        <button className='bg-gray-100 hover:bg-gray-200 px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Añadir</button>
+                    </form>
+                </div>
             </div>
-        </div>
         </>
     );
 };
--- src/app/perfil/(perfil)/components/profesional/anadir/anadirRecorrido.tsx
+++ src/app/perfil/(perfil)/components/profesional/anadir/anadirRecorrido.tsx
@@ -18,41 +18,45 @@ const anadirRecorrido: FC<anadirRecorridoProps> = ({ setIsRecorridoSelected }) =
     }, [setIsRecorridoSelected]);
 
     return (
-        <div className='flex flex-col bg-white text-gray-500 px-5  mx-auto mt-36 pb-16 border border-gray-50 shadow-xl rounded-md'>
-            <div className="flex justify-end py-6 pr-2 ">
+        <>
+            <div className='w-screen bg-black bg-opacity-70' style={{ height: '2500px' }}
+                onClick={() => { setIsRecorridoSelected(false) }}>
+            </div>
+            <div className='flex flex-col bg-white text-gray-500 px-5  mx-auto mt-36 pb-16 border border-gray-50 shadow-xl rounded-md' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>            <div className="flex justify-end py-6 pr-2 ">
                 <svg className="h-8 w-8 cursor-pointer text-gray-300 hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"
                     onClick={() => { setIsRecorridoSelected(false) }}
                 >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </div>
-            <div className='px-20'>
-                <p className='text-xl font-medium pb-5'>Añadir experiencia laboral</p>
-                <form className='flex flex-col px-5'>
-                    <label> Cargo</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='Oficial de 1a' />
-                    <label> Empresa</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='VIDRIOPERFIL.COM  ' />
-                    <label> Desde</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='Año de inicio' />
-                    <label> Hasta</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='Año de finalización' />
-                    <label> Lugar</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='Barcelona' />
-                    <label> Descripción</label>
-                    <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
-                        placeholder='3 años de experiencia como oficial de 1a en línea de doble acristalamiento' />
+                <div className='px-20'>
+                    <p className='text-xl font-medium pb-5'>Añadir experiencia laboral</p>
+                    <form className='flex flex-col px-5'>
+                        <label> Cargo</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Oficial de 1a' />
+                        <label> Empresa</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='VIDRIOPERFIL.COM  ' />
+                        <label> Desde</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Año de inicio' />
+                        <label> Hasta</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Año de finalización' />
+                        <label> Lugar</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='Barcelona' />
+                        <label> Descripción</label>
+                        <input className='bg-white p-2 px-4 mt-1 mb-5 rounded-lg border border-gray-100 shadow placeholder-gray-300'
+                            placeholder='3 años de experiencia como oficial de 1a en línea de doble acristalamiento' />
 
-                    <button className='bg-gray-100 hover:bg-gray-200 px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Añadir</button>
-                </form>
+                        <button className='bg-gray-100 hover:bg-gray-200 px-4 py-2 rounded-md shadow text-gray-500 text-xs'>Añadir</button>
+                    </form>
+                </div>
             </div>
-        </div>
-        )
+        </>
+    )
 }
 
 export default anadirRecorrido
