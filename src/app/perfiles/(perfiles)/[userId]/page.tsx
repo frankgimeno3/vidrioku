aefd799114986cb3dc7ca60aca07cab37073817e
frank gimeno <frankgimeno3@gmail.com>
2024-02-15 21:07:10 +0100

    creo perfilES para ver otros perfiles

---
+++ src/app/perfiles/(perfiles)/[userId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <p>Se pilla user id por params, se hace fetch, y pillamos el type</p>
+        <p>Renderizar una cosa u otra dependiendo de lo que sea</p>
+        <p>Lo mismo que en perfil, pero sin botones de editar</p>
+    </div>
+  )
+}
+
+export default page
