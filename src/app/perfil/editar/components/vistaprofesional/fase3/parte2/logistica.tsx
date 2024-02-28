36896effbfa42328a112665cafa5b11bd39165c4
frank gimeno <frankgimeno3@gmail.com>
2024-02-28 15:06:44 +0100

    departamento logistica y calidad actualizado

--- src/app/perfil/editar/components/vistaprofesional/fase3/parte2/logistica.tsx
+++ src/app/perfil/editar/components/vistaprofesional/fase3/parte2/logistica.tsx
@@ -19,20 +19,24 @@ interface User {
 
 const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
 
-  const [compraMaquinaria, setCompraMaquinaria] = useState(false);
-  const [estudiosCompraMaquinaria, setEstudiosCompraMaquinaria] = useState('');
-  const [detallesCompraMaquinaria, setDetallesCompraMaquinaria] = useState('');
+  const [instalador, setInstalador] = useState(false);
+  const [estudiosInstalador, setEstudiosInstalador] = useState('');
+  const [detallesInstalador, setDetallesInstalador] = useState('');
+ 
+  const [transportista, setTransportista] = useState(false);
+  const [estudiosTransportista, setEstudiosTransportista] = useState('');
+  const [detallesTransportista, setDetallesTransportista] = useState('');
 
-  const [compraSuministros, setCompraSuministros] = useState(false);
-  const [estudiosCompraSuministros, setEstudiosCompraSuministros] = useState('');
-  const [detallesCompraSuministros, setDetallesCompraSuministros] = useState('');
+  const [almacenManipulacionInterna, setAlmacenManipulacionInterna] = useState(false);
+  const [estudiosAlmacenManipulacionInterna, setEstudiosAlmacenManipulacionInterna] = useState('');
+  const [detallesAlmacenManipulacionInterna, setDetallesAlmacenManipulacionInterna] = useState('');
 
-  const [otrasCompras, setOtrasCompras] = useState(false);
-  const [estudiosOtrasCompras, setEstudiosOtrasCompras] = useState('');
-  const [detallesOtrasCompras, setDetallesOtrasCompras] = useState('');
+  const [gestionStockLogisticaExpediciones, setGestionStockLogisticaExpediciones] = useState(false);
+  const [estudiosGestionStockLogisticaExpediciones, setEstudiosGestionStockLogisticaExpediciones] = useState('');
+  const [detallesGestionStockLogisticaExpediciones, setDetallesGestionStockLogisticaExpediciones] = useState('');
 
-  const [arrayPosicionesLogisticaProps, setArrayPosicionesLogisticaProps] = useState<string[]>([]);
-  const [detallePosicionesLogisticaProps, setDetallePosicionesLogisticaProps] = useState<ExperienciaItem[]>([]);
+  const [arrayPosicionesLogistica, setArrayPosicionesLogistica] = useState<string[]>([]);
+  const [detallePosicionesLogistica, setDetallePosicionesLogistica] = useState<ExperienciaItem[]>([]);
   const [arrayPosicionesEliminar, setArrayPosicionesEliminar] = useState<string[]>([])
 
 
@@ -46,31 +50,38 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
   useEffect(() => {
     if (receivedUser) {
       let detallesPosicionesRecibido = receivedUser.posicionesMap.detallePosicionesArray;
-      if (receivedUser.posicionesMap.arrayPosiciones.includes('compraMaquinaria')) {
-        setCompraMaquinaria(true);
-        let compraMaquinariaDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'compraMaquinaria');
-        if (compraMaquinariaDetails) {
-          setEstudiosCompraMaquinaria(compraMaquinariaDetails.estudios);
-          setDetallesCompraMaquinaria(compraMaquinariaDetails.detalles);
+      if (receivedUser.posicionesMap.arrayPosiciones.includes('instalador')) {
+        setInstalador(true);
+        let instaladorDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'instalador');
+        if (instaladorDetails) {
+          setEstudiosInstalador(instaladorDetails.estudios);
+          setDetallesInstalador(instaladorDetails.detalles);
         }
       }
-      if (receivedUser.posicionesMap.arrayPosiciones.includes('compraSuministros')) {
-        setCompraSuministros(true);
-        let compraSuministrosDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'compraSuministros');
-        if (compraSuministrosDetails) {
-          setEstudiosCompraSuministros(compraSuministrosDetails.estudios);
-          setDetallesCompraSuministros(compraSuministrosDetails.detalles);
+      if (receivedUser.posicionesMap.arrayPosiciones.includes('transportista')) {
+        setTransportista(true);
+        let transportistaDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'transportista');
+        if (transportistaDetails) {
+          setEstudiosTransportista(transportistaDetails.estudios);
+          setDetallesTransportista(transportistaDetails.detalles);
         }
       }
-      if (receivedUser.posicionesMap.arrayPosiciones.includes('otrasCompras')) {
-        setOtrasCompras(true);
-        let otrasComprasDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'otrasCompras');
-        if (otrasComprasDetails) {
-          setEstudiosOtrasCompras(otrasComprasDetails.estudios);
-          setDetallesOtrasCompras(otrasComprasDetails.detalles);
+      if (receivedUser.posicionesMap.arrayPosiciones.includes('almacenManipulacionInterna')) {
+        setAlmacenManipulacionInterna(true);
+        let almacenManipulacionInternaDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'almacenManipulacionInterna');
+        if (almacenManipulacionInternaDetails) {
+          setEstudiosAlmacenManipulacionInterna(almacenManipulacionInternaDetails.estudios);
+          setDetallesAlmacenManipulacionInterna(almacenManipulacionInternaDetails.detalles);
         }
       }
-
+      if (receivedUser.posicionesMap.arrayPosiciones.includes('gestionStockLogisticaExpediciones')) {
+        setGestionStockLogisticaExpediciones(true);
+        let gestionStockLogisticaExpedicionesDetails = detallesPosicionesRecibido.find((obj: { posicion: string; }) => obj.posicion === 'gestionStockLogisticaExpediciones');
+        if (gestionStockLogisticaExpedicionesDetails) {
+          setEstudiosGestionStockLogisticaExpediciones(gestionStockLogisticaExpedicionesDetails.estudios);
+          setDetallesGestionStockLogisticaExpediciones(gestionStockLogisticaExpedicionesDetails.detalles);
+        }
+      }
     }
   }, [receivedUser]);
 
@@ -90,46 +101,54 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
    }
 
   useEffect(() => {
-    const actualizarPosicionesCompras = () => {
-      const posicionesComprasArrayOperativo = [];
-      const detallePosicionesComprasArrayOperativo = [];
-      if (compraMaquinaria) {
-        posicionesComprasArrayOperativo.push('compraMaquinaria');
-        detallePosicionesComprasArrayOperativo.push({
-          posicion: 'compraMaquinaria',
-          estudios: estudiosCompraMaquinaria,
-          detalles: detallesCompraMaquinaria
+    const actualizarPosicionesLogistica = () => {
+      const posicionesLogisticaArrayOperativo = [];
+      const detallePosicionesLogisticaArrayOperativo = [];
+      if (instalador) {
+        posicionesLogisticaArrayOperativo.push('instalador');
+        detallePosicionesLogisticaArrayOperativo.push({
+          posicion: 'instalador',
+          estudios: estudiosInstalador,
+          detalles: detallesInstalador
         })
       }
-      if (compraSuministros) {
-        posicionesComprasArrayOperativo.push('compraSuministros');
-        detallePosicionesComprasArrayOperativo.push({
-          posicion: 'compraSuministros',
-          estudios: estudiosCompraSuministros,
-          detalles: detallesCompraSuministros
+      if (transportista) {
+        posicionesLogisticaArrayOperativo.push('transportista');
+        detallePosicionesLogisticaArrayOperativo.push({
+          posicion: 'transportista',
+          estudios: estudiosTransportista,
+          detalles: detallesTransportista
         })
       }
-      if (otrasCompras) {
-        posicionesComprasArrayOperativo.push('otrasCompras');
-        detallePosicionesComprasArrayOperativo.push({
-          posicion: 'otrasCompras',
-          estudios: estudiosOtrasCompras,
-          detalles: detallesOtrasCompras
+      if (almacenManipulacionInterna) {
+        posicionesLogisticaArrayOperativo.push('almacenManipulacionInterna');
+        detallePosicionesLogisticaArrayOperativo.push({
+          posicion: 'almacenManipulacionInterna',
+          estudios: estudiosAlmacenManipulacionInterna ,
+          detalles: detallesAlmacenManipulacionInterna
         })
       }
-
-      setArrayPosicionesCompras(posicionesComprasArrayOperativo);
-      setDetallePosicionesCompras(detallePosicionesComprasArrayOperativo)
+      if (gestionStockLogisticaExpediciones) {
+        posicionesLogisticaArrayOperativo.push('gestionStockLogisticaExpediciones');
+        detallePosicionesLogisticaArrayOperativo.push({
+          posicion: 'gestionStockLogisticaExpediciones',
+          estudios: estudiosGestionStockLogisticaExpediciones,
+          detalles: detallesGestionStockLogisticaExpediciones
+        })
+      }
+      setArrayPosicionesLogistica(posicionesLogisticaArrayOperativo);
+      setDetallePosicionesLogistica(detallePosicionesLogisticaArrayOperativo)
     }
 
-    actualizarPosicionesCompras();
-  }, [compraMaquinaria, estudiosCompraMaquinaria, detallesCompraMaquinaria,
-    compraSuministros, estudiosCompraSuministros, detallesCompraSuministros,
-    otrasCompras, estudiosOtrasCompras, detallesOtrasCompras]
+    actualizarPosicionesLogistica();
+  }, [instalador, estudiosInstalador, detallesInstalador,
+    transportista, estudiosTransportista, detallesTransportista,
+    almacenManipulacionInterna, estudiosAlmacenManipulacionInterna, detallesAlmacenManipulacionInterna,
+    gestionStockLogisticaExpediciones, estudiosGestionStockLogisticaExpediciones, detallesGestionStockLogisticaExpediciones]
   );
 
 
-  const ActualizarPosicionesCompras = async () => { 
+  const ActualizarPosicionesLogistica = async () => { 
 
     try {
       const docRef = doc(db, "users", receivedUser.id);
@@ -153,8 +172,8 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
           }
         }
 
-        if (arrayPosicionesCompras.length !== 0) {
-          for (const item of arrayPosicionesCompras) {
+        if (arrayPosicionesLogistica.length !== 0) {
+          for (const item of arrayPosicionesLogistica) {
             const index = arrayPosicionesToUpdate.findIndex((existingItem: string) => existingItem === item);
             if (index !== -1) {
               arrayPosicionesToUpdate[index] = item;
@@ -164,8 +183,8 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
           }
         }
 
-        if (detallePosicionesCompras.length !== 0) {
-          for (const newItem of detallePosicionesCompras) {
+        if (detallePosicionesLogistica.length !== 0) {
+          for (const newItem of detallePosicionesLogistica) {
             const index = detallePosicionesToUpdate.findIndex((existingItem: { posicion: string; }) => existingItem.posicion === newItem.posicion);
             if (index !== -1) {
               detallePosicionesToUpdate[index] = newItem;
@@ -193,33 +212,33 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
   };
 
   const handleGuardarYseguir = () => {
-    ActualizarPosicionesCompras()
-    cambioComponenteMostrar('compras');
+    ActualizarPosicionesLogistica()
+    cambioComponenteMostrar('logistica');
   };
   return (
     <div>
       <div>
-        <p className='pt-8 text-lg font-bold'> Departamento de Compras</p>
+        <p className='pt-8 text-lg font-bold'> Departamentos de almacenamiento, logística y transportes</p>
         <p>Haga click en una de las siguientes opciones si ha trabajado en dicha posición, o tiene estudios relacionados con dicho departamento</p>
         <div className='flex flex-col p-2'>
           <div className='py-1 pl-5'>
             <label className='flex flex-row items-center'  >
-              <input type="checkbox" onChange={() => handleExperienceToggle(compraMaquinaria, 'compraMaquinaria', setCompraMaquinaria)} />
+              <input type="checkbox" onChange={() => handleExperienceToggle(instalador, 'instalador', setInstalador)} />
               <p className='ml-2'> Instalación de vidrios y/o ventanas</p>
             </label>
-            {compraMaquinaria &&
+            {instalador &&
               <div className='flex flex-col'>
                 <textarea 
                   placeholder="Estudios relacionados"
-                  value={estudiosCompraMaquinaria}
-                  onChange={(e) => setEstudiosCompraMaquinaria(e.target.value)}
+                  value={estudiosInstalador}
+                  onChange={(e) => setEstudiosInstalador(e.target.value)}
                   className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'>
                   </textarea>
 
                   <textarea 
                   placeholder="Detalles de la experiencia"
-                  value={detallesCompraMaquinaria}
-                  onChange={(e) => setDetallesCompraMaquinaria(e.target.value)}
+                  value={detallesInstalador}
+                  onChange={(e) => setDetallesInstalador(e.target.value)}
                   className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'>
                     </textarea>
               </div>}
@@ -227,37 +246,54 @@ const Logistica: FC<LogisticaProps> = ({ user, cambioComponenteMostrar }) => {
 
           <div className='py-1 pl-5'>
             <label className='flex flex-row items-top'  >
-              <input type="checkbox" onChange={() => handleExperienceToggle(compraSuministros, 'compraSuministros', setCompraSuministros)}
+              <input type="checkbox" onChange={() => handleExperienceToggle(transportista, 'transportista', setTransportista)}
                 className='my-1' />
-              <p className='ml-2 text-left'>Compra de suministros para fabricación y/o manipulado de vidrio</p>
+              <p className='ml-2 text-left'>Transportista de vidrios y/o ventanas fuera de fábrica</p>
             </label>
-            {compraSuministros && <div className='flex flex-col'>
+            {transportista && <div className='flex flex-col'>
               <textarea
                 placeholder="Estudios relacionados"
-                value={estudiosCompraSuministros}
-                onChange={(e) => setEstudiosCompraSuministros(e.target.value)}
+                value={estudiosTransportista}
+                onChange={(e) => setEstudiosTransportista(e.target.value)}
                 className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'>
                 </textarea>
               <textarea placeholder="Estudios relacionados"
-                value={detallesCompraSuministros}
-                onChange={(e) => setDetallesCompraSuministros(e.target.value)}
+                value={detallesTransportista}
+                onChange={(e) => setDetallesTransportista(e.target.value)}
                 className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'></textarea>
             </div>}
           </div>
 
           <div className='py-1 pl-5'>
             <label className='flex flex-row items-center'  >
-              <input type="checkbox" onChange={() => handleExperienceToggle(otrasCompras, 'otrasCompras', setOtrasCompras)} />
-              <p className='ml-2'>Otras experiencias en compras</p>
+              <input type="checkbox" onChange={() => handleExperienceToggle(almacenManipulacionInterna, 'almacenManipulacionInterna', setAlmacenManipulacionInterna)} />
+              <p className='ml-2'>Profesionales de almacén o manipulación interna de vidrios y/o otros bienes</p>
             </label>
-            {otrasCompras && <div className='flex flex-col'>
+            {almacenManipulacionInterna && <div className='flex flex-col'>
               <textarea placeholder="Estudios relacionados"
-                value={estudiosOtrasCompras}
-                onChange={(e) => setEstudiosOtrasCompras(e.target.value)}
+                value={estudiosAlmacenManipulacionInterna}
+                onChange={(e) => setEstudiosAlmacenManipulacionInterna(e.target.value)}
                 className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'></textarea>
               <textarea placeholder="Detalles de la experiencia"
-                value={detallesOtrasCompras}
-                onChange={(e) => setDetallesOtrasCompras(e.target.value)}
+                value={detallesAlmacenManipulacionInterna}
+                onChange={(e) => setDetallesAlmacenManipulacionInterna(e.target.value)}
+                className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'></textarea>
+            </div>}
+          </div>
+
+          <div className='py-1 pl-5'>
+            <label className='flex flex-row items-center'  >
+              <input type="checkbox" onChange={() => handleExperienceToggle(gestionStockLogisticaExpediciones, 'gestionStockLogisticaExpediciones', setGestionStockLogisticaExpediciones)} />
+              <p className='ml-2'>Gestión de stock, logística y expediciones</p>
+            </label>
+            {gestionStockLogisticaExpediciones && <div className='flex flex-col'>
+              <textarea placeholder="Estudios relacionados"
+                value={estudiosGestionStockLogisticaExpediciones}
+                onChange={(e) => setEstudiosGestionStockLogisticaExpediciones(e.target.value)}
+                className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'></textarea>
+              <textarea placeholder="Detalles de la experiencia"
+                value={detallesGestionStockLogisticaExpediciones}
+                onChange={(e) => setDetallesGestionStockLogisticaExpediciones(e.target.value)}
                 className='my-1 border-gray-100 placeholder-gray-400 rounded-lg'></textarea>
             </div>}
           </div>
