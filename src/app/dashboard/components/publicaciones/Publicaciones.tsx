8ea8f01b6aa875490eba31670224754956e51491
frank gimeno <frankgimeno3@gmail.com>
2024-05-31 21:36:28 +0200

    creo componentes publicaciones y sin seguimientos

---
+++ src/app/dashboard/components/publicaciones/Publicaciones.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface PublicacionesProps {
+  
+}
+
+const Publicaciones: FC<PublicacionesProps> = ({ }) => {
+  return (
+    <div>Publicaciones</div>
+  );
+};
+
+export default Publicaciones;
---
+++ src/app/dashboard/components/publicaciones/SinSeguimientos.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface SinSeguimientosProps {
+  
+}
+
+const SinSeguimientos: FC<SinSeguimientosProps> = ({ }) => {
+  return (
+    <div>SinSeguimientos</div>
+  );
+};
+
+export default SinSeguimientos;
