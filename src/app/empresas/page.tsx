f44a66ab5b44be4f33eb1886979e6e505cad45af
frank gimeno <frankgimeno3@gmail.com>
2024-06-08 02:22:55 +0200

    añado user centralizado a empresas

--- src/app/empresas/page.tsx
+++ src/app/empresas/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux';
+import useUserSession from '../components/hooks/userSession';
+import { selectUser } from '@/redux/features/userSlice';
 
 function empresas() {
+  const { userData, session } = useUserSession();
+  const user = useSelector(selectUser);
   return (
     <div>empresas</div>
   )
