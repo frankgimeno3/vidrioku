c99747244d647cac54abb3aaa98055f3623b2fd0
frank gimeno <frankgimeno3@gmail.com>
2024-06-08 02:04:48 +0200

    corrijo pequeño error tipado userData

--- src/app/components/hooks/userSession.ts
+++ src/app/components/hooks/userSession.ts
@@ -7,10 +7,16 @@ import { db } from '../../firebase';
 import { updateUser } from '@/redux/features/userSlice';
 import { User } from '../interfaces/interfaces'; 
 
-const useUserSession = () => {
+interface UserSession {
+  userData: User | null; // Ensure userData is a User object or null
+  session: any; // Adjust the type as needed
+  userType: string;
+}
+
+const useUserSession = (): UserSession => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const [userData, setUserData] = useState<string | null>(null); // Ensure userData is a string or null
+  const [userData, setUserData] = useState<User | null>(null); // Ensure userData is a User object or null
   const [userType, setUserType] = useState<string>('');
 
   const session = useSession({
@@ -21,28 +27,22 @@ const useUserSession = () => {
   });
 
   useEffect(() => {
-    if (session?.data?.user?.email) {
-      setUserData(session.data.user.email);
-    } else {
-      setUserData("Usuario");
-    }
-  }, [session?.data?.user?.email]);
-
-  useEffect(() => {
     const fetchDoc = async () => {
-      if (userData) {
-        const docRef = doc(db, "users", userData);  
+      if (session?.data?.user?.email) {
+        const docRef = doc(db, "users", session.data.user.email);  
         const response = await getDoc(docRef);
         if (response.exists()) {
           const myUserData = response.data() as User;
           dispatch(updateUser(myUserData));
           setUserType(myUserData.userType);
-           setUserData(myUserData.email || "Usuario");
+          setUserData(myUserData);
         }
+      } else {
+        setUserData(null);
       }
     };
     fetchDoc();
-  }, [userData, dispatch]);
+  }, [session?.data?.user?.email, dispatch]);
 
   return { userData, session, userType };
 };
